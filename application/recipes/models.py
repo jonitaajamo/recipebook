ac1481be6d41e91ea7c6a28c683e60ba297cc30c
jonitaajamo <joni.taajamo@outlook.com>
2019-02-07 10:27:41 +0200

    now only public recipes are listed

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -1,20 +1,17 @@
 from application import db
+from application.models import Base
 
 # TODO: all tables
 
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+class Recipe(Base):
     
     name = db.Column(db.String(144), nullable=False)
     recipe_text = db.Column(db.String(1000), nullable=False)
     tips = db.Column(db.String(500), nullable=True)
     public = db.Column(db.Boolean, nullable=False)
     votes = db.Column(db.Integer, nullable=False)
-    
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
-                           nullable=False)
+
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
     def __init__(self, name, recipe_text, tips):
         self.name = name
